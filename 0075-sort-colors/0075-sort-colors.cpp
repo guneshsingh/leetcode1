29e55e0f1f963665e502623b093b564e24a0ff34
Gunesh Kumar <guneshsingh30@gmail.com>
2026-07-14 20:58:07 +0530

    Time: 0 ms (100%), Space: 11.7 MB (11.63%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-           }
+            vector<int> count0;
+        vector<int> count1;
+        vector<int> count2;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+            count0.push_back(nums[i]);
+            else if(nums[i]==1)
+            count1.push_back(nums[i]);
+            else
+            count2.push_back(nums[i]);
+        }
+        int j=0;
+        for(int i=0;i<count0.size();i++){
+            nums[j]=count0[i];
+            j++;
+        }
+        for(int i=0;i<count1.size();i++){
+            nums[j]=count1[i];
+            j++;
+        }
+        for(int i=0;i<count2.size();i++){
+            nums[j]=count2[i];
+            j++;
+        }}
 };
