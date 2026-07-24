7febf132045d740cfd49c4ddd4aa5b884a655e78
Gunesh Kumar <guneshsingh30@gmail.com>
2026-07-24 11:33:28 +0530

    Time: 3 ms (72.27%), Space: 52.8 MB (29.35%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,45 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> h=new HashMap<>();
-        List<Integer> a=new ArrayList<>();
-        int m=nums.length/3 ;
-        for(int i: nums){
-            h.put(i,h.getOrDefault(i,0)+1);
-        }
+        int count1=0,count2=0, el1=0,el2=0;
+        List<Integer> ls=new ArrayList<>();
         for(int i=0;i<nums.length;i++){
-           
-            if(h.get(nums[i])>m && !a.contains(nums[i])){
-                a.add(nums[i]);
+            if(count1==0 && el2!=nums[i]){
+                count1+=1;
+                el1=nums[i];
             }
-        }   return a;
+            else if(count2==0 && el1!=nums[i]){
+                count2+=1;
+                el2=nums[i];
+            }
+            else if(el1==nums[i]){
+            count1++;
+            }
+            else if(el2==nums[i]){
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+           
+        }
+        count1=0;
+        count2=0;
+        for(int i:nums){
+            if(i==el1)
+            count1++;
+            else if(i==el2)
+            count2++;
+        }
+        
+
+        if (count1 > nums.length / 3)
+            ls.add(el1);
+
+        if (count2 > nums.length / 3)
+            ls.add(el2);
+
+        return ls;
+        
     }
 }
