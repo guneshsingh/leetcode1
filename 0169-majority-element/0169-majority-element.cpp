c469397417c4dbd967f0967d1ecdbeb1c5dc05a8
Gunesh Kumar <guneshsingh30@gmail.com>
2026-07-30 00:59:27 +0530

    Time: 0 ms (100%), Space: 28.2 MB (71.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -15,14 +15,14 @@ public:
                 c--;
             }
         }
-        int cnt1=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==ele)
-            cnt1++;
-        }
-        if(cnt1>nums.size()/2)
+        // int cnt1=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==ele)
+        //     cnt1++;
+        // }
+        // if(cnt1>nums.size()/2)
         return ele;
-        return -1;
+        // return -1;
     }
     
 };
