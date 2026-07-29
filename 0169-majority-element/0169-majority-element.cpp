1c5381f598d927836a61837707af68a480067561
Gunesh Kumar <guneshsingh30@gmail.com>
2026-07-30 00:53:45 +0530

    Time: 4 ms (18.83%), Space: 28.2 MB (71.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-    //     int c=0;
-    //     int ele;
-    //     for(int i=0;i<nums.size();i++){
-    //         if(c==0){
-    //             c=1;
-    //             ele=nums[i];
-    //         }
-    //         else if(nums[i]==ele){
-    //             c++;
-    //         }
-    //         else{
-    //             c--;
-    //         }
-    //     }
-    //     int cnt1=0;
-    //     for(int i=0;i<nums.size();i++){
-    //         if(nums[i]==ele)
-    //         cnt1++;
-    //     }
-    //     if(cnt1>nums.size()/2)
-    //     return ele;
-    //     return -1;
-    // }
-    unordered_map<int,int>mp;
-    for(int i=0;i<nums.size();i++){
-        mp[nums[i]]++;
-        if(mp[nums[i]]>ceil(nums.size()/2)){
-            return nums[i];
+        int c=0;
+        int ele;
+        for(int i=0;i<nums.size();i++){
+            if(c==0){
+                c=1;
+                ele=nums[i];
+            }
+            else if(nums[i]==ele){
+                c++;
+            }
+            else{
+                c--;
+            }
         }
+        int cnt1=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==ele)
+            cnt1++;
+        }
+        if(cnt1>nums.size()/2)
+        return ele;
+        return -1;
     }
-    return -1;
-    }
+    
 };
