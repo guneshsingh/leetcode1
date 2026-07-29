86deb7215ab15a763fb044bdbe755e92d6b602d6
Gunesh Kumar <guneshsingh30@gmail.com>
2026-07-29 23:51:31 +0530

    Time: 0 ms (100%), Space: 25.7 MB (82.07%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        long long n=grid.size()*grid.size();
+        long long s=0,s2=0;
+        long long sn=(n*(n+1))/2;
+        long long s2n=(n*(n+1)*(2*n+1))/6;
         
-        int miss=-1,dou=-1;
-        for(int i=1;i<=grid.size()*grid.size();i++){
-            int count=0;
+        for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid.size();j++){
-                for(int k=0;k<grid.size();k++)
-                    {if(grid[j][k]==i)
-                        {count++;}
-                    
-            }}
-            if(count==2) dou=i;
-            else if(count==0) miss=i;
+                s+=grid[i][j];
+                s2+=grid[i][j]*grid[i][j];
+            }
         }
+        long long first=s-sn;
+        long long second=s2-s2n;
+        second=second/first;
+        long long x=(first+second)/2;
+        long long y=x-first;
+        return {(int)x,(int)y};
         
-        
-        return {dou,miss};
     }
 };
