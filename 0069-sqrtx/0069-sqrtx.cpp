5582882b69e1a456d63354b8070c63d82b464aeb
Gunesh Kumar <guneshsingh30@gmail.com>
2026-08-10 22:40:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.94%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        double y=sqrt(x);
-        return y;
+        
+        int l=1,h=x;
+        while(l<=h){
+            long long mid=l+(h-l)/2;
+            long long val={mid*mid};
+            if(val<=x)
+            {
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+            }
+            return h;
     }
 };
